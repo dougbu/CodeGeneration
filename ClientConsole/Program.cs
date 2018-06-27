15e179fd192c7db074e3e44bac032751ac2b141e
Doug Bunting <dougbu@microsoft.com>
2018-06-27 16:41:59 -0700

    Initial files

---
+++ ClientConsole/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ClientConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
