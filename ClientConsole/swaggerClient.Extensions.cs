e9e79566b54b7deab71165421d71356fde571be6
Doug Bunting <dougbu@microsoft.com>
2018-07-18 10:37:36 -0700

    Add more files

---
+++ ClientConsole/swaggerClient.Extensions.cs
@@ -0,0 +1,12 @@
+﻿using System.Net.Http;
+
+namespace ClientConsole
+{
+    public partial class swaggerClient
+    {
+        partial void PrepareRequest(HttpClient client, HttpRequestMessage request, string url)
+        {
+            // no-op
+        }
+    }
+}
