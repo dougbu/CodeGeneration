62d7772e150f293d9aa7a5b58d64e15bfac62108
Doug Bunting <dougbu@microsoft.com>
2018-08-02 11:59:52 -0700

    Initial files for document generation tool and its inside man - copied from aspnet/EntityFrameworkCore

---
+++ GetDocumentInsider/Commands/DbContextInfoCommand.Configure.cs
@@ -0,0 +1,22 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using GetDocument.Insider.Properties;
+using Microsoft.DotNet.Cli.CommandLine;
+
+namespace GetDocument.Insider.Commands
+{
+    internal partial class DbContextInfoCommand : ContextCommandBase
+    {
+        private CommandOption _json;
+
+        public override void Configure(CommandLineApplication command)
+        {
+            command.Description = Resources.DbContextInfoDescription;
+
+            _json = Json.ConfigureOption(command);
+
+            base.Configure(command);
+        }
+    }
+}
---
+++ GetDocumentInsider/Commands/MigrationsListCommand.Configure.cs
@@ -0,0 +1,22 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using GetDocument.Insider.Properties;
+using Microsoft.DotNet.Cli.CommandLine;
+
+namespace GetDocument.Insider.Commands
+{
+    internal partial class MigrationsListCommand : ContextCommandBase
+    {
+        private CommandOption _json;
+
+        public override void Configure(CommandLineApplication command)
+        {
+            command.Description = Resources.MigrationsListDescription;
+
+            _json = Json.ConfigureOption(command);
+
+            base.Configure(command);
+        }
+    }
+}
