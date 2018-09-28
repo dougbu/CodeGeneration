1fe12735bbd5e80f22a38138908e5e5090d3a88a
Doug Bunting <dougbu@microsoft.com>
2018-09-28 11:02:36 -0700

    Correct `SwaggerClient` class name in `SwaggerClient.Extensions.cs` - rename file to match

--- ClientConsole/SwaggerClient.Extensions.cs
+++ ClientConsole/SwaggerClient.Extensions.cs
@@ -2,7 +2,7 @@
 
 namespace ClientConsole
 {
-    public partial class swaggerClient
+    public partial class SwaggerClient
     {
         partial void PrepareRequest(HttpClient client, HttpRequestMessage request, string url)
         {
